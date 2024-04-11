ea55a935023f4ffa3e99038c843c31ba48a6026b
NaNajli <nadurodriguez2@gmail.com>
2024-04-10 23:55:29 -0300

    Improving

--- portfolio/scripts/darkmode.js
+++ portfolio/scripts/darkmode.js
@@ -3,25 +3,29 @@
 
 const modeButton = document.querySelector("#mode");
 const main = document.querySelector("main");
-const html = document.querySelector("html")
+const html = document.querySelector("html");
+const dark = document.querySelector(".glasses");
 
 modeButton.addEventListener("click", () => {
-    if (modeButton.textContent.includes("🕶️")) {
-        html.style.background = "#121212";
-        main.style.color = "#f5cac3";
-
-
-        modeButton.textContent = "💡";
-    } else if (modeButton.textContent.includes("💡")) {
+    if (modeButton.textContent.includes("💡")) {
         html.style.background = "#f5cac3";
         main.style.color = "#242424";
+        modeButton.textContent = "🩷";
 
-        modeButton.textContent = "🕶️";
-    }
-    else {
-        html.style.background = "#f5cac3";
+    } else {
+        html.style.background = "#121212";
+        main.style.color = "#f5cac3";
+        modeButton.textContent = "💡";
 
     }
+
 });
+
+
+
+
+
+
+
 
 
